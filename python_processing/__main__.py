eff6f9699845b95c3c7c5b35885b644dd7b2f7b9
iCalculated <fufa0001@gmail.com>
2022-02-05 15:14:23 -0600

    routes!

--- python_processing/__main__.py
+++ python_processing/__main__.py
@@ -3,23 +3,3 @@ import python_processing.app as app
 import os
 
 uvicorn.run(app.app, host="127.0.0.1", port=int(os.environ.get("PORT", 8000)))
-
-@app.on_event("start_up")
-def start_up():
-  print("Starting server.")
-
-@app.get("/")
-async def root():
-  return "Hello world!"
-
-@app.get("/process_media/{uri}")
-def process_video(uri: str):
-  """
-  processes a given video, returns schema TBD
-
-  uri -- gcs uri of media resource to process
-  """
-
-@app.get("/test")
-def test_schema():
-  return {"field": "value"}
