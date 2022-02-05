bd678c761b5dbc6ef07fa921c6409c385505a4a9
iCalculated <fufa0001@gmail.com>
2022-02-05 15:09:52 -0600

    initial routes

--- python_processing/__main__.py
+++ python_processing/__main__.py
@@ -3,3 +3,11 @@ import python_processing.app as app
 import os
 
 uvicorn.run(app.app, host="127.0.0.1", port=int(os.environ.get("PORT", 8000)))
+
+@app.on_event("start_up")
+def start_up():
+  print("Starting server.")
+
+@app.get("/")
+async def root():
+  return "Hello world!"
