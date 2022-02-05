16f9825dc34cfe50e519c773b3222966a8062db4
iCalculated <fufa0001@gmail.com>
2022-02-05 15:22:46 -0600

    storage client

--- python_processing/gcs.py
+++ python_processing/gcs.py
@@ -4,3 +4,4 @@ from google.oauth2 import service_account
 from pathlib import Path
 
 credentials = service_account.Credentials.from_service_account_file(Path(__file__).parent / './violet-harassment-e63f719390aa.json')
+storage_client = storage.Client(project="violet-harassment", credentials=credentials)
