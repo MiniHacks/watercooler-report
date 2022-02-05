40e2c96802a57514838d60cf9ebc031047fa0a9d
iCalculated <fufa0001@gmail.com>
2022-02-05 15:12:19 -0600

    further routes

--- python_processing/__main__.py
+++ python_processing/__main__.py
@@ -11,3 +11,15 @@ def start_up():
 @app.get("/")
 async def root():
   return "Hello world!"
+
+@app.get("/process_media/{uri}")
+def process_video(uri: str):
+  """
+  processes a given video, returns schema TBD
+
+  uri -- gcs uri of media resource to process
+  """
+
+@app.get("/test")
+def test_schema():
+  return {"field": "value"}
